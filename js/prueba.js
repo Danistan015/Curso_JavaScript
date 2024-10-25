9a690c427616772abeb21cffbf963a2b9c94830c
Danistan015 <sotograjalesdaniela@gmail.com>
2024-10-25 11:53:23 -0500

    Curso_JavaScripts

---
+++ js/prueba.js
@@ -0,0 +1,11 @@
+let nombre;
+nombre= "Daniela"
+let edad= 25;
+let esEstudiante= true;
+let coloresFav= ["rojo","azul","morado"];
+let persona= {nombre :"DAN",edad:19};
+let x=10;
+let y = 5;
+//las operaciones se manejan igual
+console.log("hola mundo")
+let edad1 = prompt("¿Cuántos años tienes?");
